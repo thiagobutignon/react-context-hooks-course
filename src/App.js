6faecb5656771c1c68dd8b933ace00217c3ea507
Thiago Butignon <thiagobutignon@gmail.com>
2020-02-12 01:18:02 -0300

    Create ThemeContextProvider with context api

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React from "react";
 import Navbar from "./components/Navbar";
 import BookList from "./components/BookList";
+import ThemeContextProvider from "./contexts/ThemeContext";
 
 function App() {
   return (
     <div className="App">
-      <Navbar></Navbar>
-      <BookList></BookList>
+      <ThemeContextProvider>
+        <Navbar />
+        <BookList />
+      </ThemeContextProvider>
     </div>
   );
 }
