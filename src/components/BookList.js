19532295ee30d17d921ca47049e1b1ac47632086
Thiago Butignon <thiagobutignon@gmail.com>
2020-02-12 00:56:07 -0300

    Create Booklist and Navbar component, delete some files

---
+++ src/components/BookList.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+class BookList extends Component {
+  render() {
+    return (
+      <div className="book-list">
+        <ul>
+          <li>The way of kings</li>
+          <li>The name of wind</li>
+          <li>No fundo do oceano os animais invisíveis</li>
+        </ul>
+      </div>
+    );
+  }
+}
+
+export default BookList;
---
+++ src/components/Navbar.js
@@ -0,0 +1,18 @@
+import React, { Component } from "react";
+
+class Navbar extends Component {
+  render() {
+    return (
+      <nav>
+        <h1>ContextApp</h1>
+        <ul>
+          <li>Home</li>
+          <li>About</li>
+          <li>Contact</li>
+        </ul>
+      </nav>
+    );
+  }
+}
+
+export default Navbar;
