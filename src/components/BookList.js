31fb7f06208d9b1d7442254a6288fd87749b9ada
Thiago Butignon <thiagobutignon@gmail.com>
2020-02-12 01:37:14 -0300

    Implements themecontext consumer.

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -1,14 +1,28 @@
 import React, { Component } from "react";
+import { ThemeContext } from "../contexts/ThemeContext";
 class BookList extends Component {
   render() {
     return (
-      <div className="book-list">
-        <ul>
-          <li>The way of kings</li>
-          <li>The name of wind</li>
-          <li>No fundo do oceano os animais invisíveis</li>
-        </ul>
-      </div>
+      <ThemeContext.Consumer>
+        {context => {
+          const { isLightTheme, light, dark } = context;
+          const theme = isLightTheme ? light : dark;
+          return (
+            <div
+              className="book-list"
+              style={{ color: theme.syntax, background: theme.bg }}
+            >
+              <ul>
+                <li style={{ background: theme.ui }}>The way of kings</li>
+                <li style={{ background: theme.ui }}>The name of wind</li>
+                <li style={{ background: theme.ui }}>
+                  No fundo do oceano os animais invisíveis
+                </li>
+              </ul>
+            </div>
+          );
+        }}
+      </ThemeContext.Consumer>
     );
   }
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,16 +1,25 @@
 import React, { Component } from "react";
-
+import { ThemeContext } from "../contexts/ThemeContext";
 class Navbar extends Component {
+  static contextType = ThemeContext;
   render() {
     return (
-      <nav>
-        <h1>ContextApp</h1>
-        <ul>
-          <li>Home</li>
-          <li>About</li>
-          <li>Contact</li>
-        </ul>
-      </nav>
+      <ThemeContext.Consumer>
+        {context => {
+          const { isLightTheme, light, dark } = context;
+          const theme = isLightTheme ? light : dark;
+          return (
+            <nav style={{ background: theme.ui, color: theme.syntax }}>
+              <h1>ContextApp</h1>
+              <ul>
+                <li>Home</li>
+                <li>About</li>
+                <li>Contact</li>
+              </ul>
+            </nav>
+          );
+        }}
+      </ThemeContext.Consumer>
     );
   }
 }
